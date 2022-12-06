0e810c5214ad1ab59eec1e94164ac87dab83fea5
Mohsen Alizadeh <mohsen@alizadeh.us>
2022-12-05 21:03:17 -0800

    implement setter cop

--- src/cop/naming/accessor_method_name.rs
+++ src/cop/naming/accessor_method_name.rs
@@ -15,27 +15,23 @@ pub fn init() {
     register_node_handler("def", on_def);
 }
 
-pub fn run(result: ParserResult) {
-    // find_defs(*result.ast.unwrap());
-}
-
-// fn find_defs(node: Node) {
-//     match node {
-//         Node::Def(n) => on_def(n),
-//         Node::Begin(n) => {
-//             for statement in n.statements {
-//                 find_defs(statement);
-//             }
-//         }
-//         _ => println!("not found"),
-//     }
-// }
-
 pub fn on_def(node: &Node, offenses: types::OffenseList) {
     if let Node::Def(node) = node {
         if node.name.starts_with("get_") {
             if None == node.args {
                 reporting::add_offense(offenses, node.name_l.begin..node.name_l.end, MSG_READER);
+            }
+        } else if node.name.starts_with("set_") {
+            if let Some(args) = &node.args {
+                if let Node::Args(args) = &**args {
+                    if args.args.len() == 1 {
+                        reporting::add_offense(
+                            offenses,
+                            node.name_l.begin..node.name_l.end,
+                            MSG_WRITER,
+                        );
+                    }
+                }
             }
         }
     }
@@ -53,6 +49,20 @@ mod tests {
     fn it_detects_get_attribute() {
         let code = "
             def get_attribute
+            end
+        ";
+
+        let offenses: types::OffenseList = &Mutex::new(vec![]);
+
+        on_def(&testing::ast(code), &offenses);
+
+        assert_eq!(reporting::total(offenses), 1);
+    }
+
+    #[test]
+    fn it_detects_set_attribute() {
+        let code = "
+            def set_attribute(aa)
             end
         ";
 
