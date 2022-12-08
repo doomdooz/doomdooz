0c92182f729586443a3d497b452198c5e022d78e
Mohsen Alizadeh <mohsen@alizadeh.us>
2022-12-08 15:17:13 -0800

    print total inspected files counter

--- src/main.rs
+++ src/main.rs
@@ -23,6 +23,8 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     cop::init();
 
     let mut args: Vec<String> = env::args().skip(1).collect();
+    let mut inspected_files: usize = 0;
+    let mut offenses: usize = 0;
 
     if args.len() == 0 {
         args.push(String::from("**/*.rb"));
@@ -34,11 +36,18 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                 let file = source::File::new(path.to_str().unwrap().to_string());
                 file.process();
                 file.print_report();
+                inspected_files += 1;
+                offenses += file.total_offenses();
             } else {
                 panic!("error while reading path");
             }
         }
     }
+
+    println!(
+        "{} files inspected, {} offenses detected, XXX offenses autocorrectable",
+        inspected_files, offenses
+    );
 
     Ok(())
 }
