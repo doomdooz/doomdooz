328dfdda3c9bf5b62165cc8a513f37bf972a4f65
Mohsen Alizadeh <mohsen@alizadeh.us>
2022-12-10 14:24:31 -0800

    rename Config struct to Cop

--- src/types.rs
+++ src/types.rs
@@ -10,14 +10,13 @@ pub type NodeHandler = fn(&Node, &source::File);
 pub type TokensHandler = fn(&Vec<Token>, &source::File);
 pub type NodeHandlersMap = Mutex<HashMap<&'static str, Vec<NodeHandler>>>;
 
-// #[derive(Default)]
-pub struct Config<'a> {
+pub struct Cop<'a> {
     pub cop_name: &'static str,
     pub enabled: bool,
     pub description: &'static str,
     pub style_guide: &'static str,
     pub include: Option<Vec<String>>,
     pub exclude: Option<Vec<String>>,
-    pub parent_config: Option<&'a Config<'a>>,
+    pub parent_config: Option<&'a Cop<'a>>,
     pub supported_styles: Option<Vec<String>>,
 }
