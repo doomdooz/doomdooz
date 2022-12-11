1617592ea862ca7b64b7060263c2fc754ae3ef3a
Mohsen Alizadeh <mohsen@alizadeh.us>
2022-12-11 13:49:08 -0800

    add config

---
+++ src/config.rs
@@ -0,0 +1,73 @@
+use merge_yaml_hash::MergeYamlHash;
+use yaml_rust::yaml::Hash;
+use yaml_rust::yaml::Yaml;
+
+pub struct Config(Hash);
+
+pub fn load() -> Config {
+    let mut hash = MergeYamlHash::new();
+
+    hash.merge(include_str!("config/default.yml"));
+    // hash.merge("cherry:\n  sweet: 1\n  tart: 2");
+
+    Config(hash.data)
+}
+
+impl Config {
+    pub fn get_array(&self, cop: &str, key: &str) -> Vec<String> {
+        // TODO: consider AllCops as fallback
+        let cop_config = &self.0[&Yaml::String(cop.to_string())];
+        // let default_config = &self.0[&Yaml::String("AllCops".to_string())];
+        let mut output: Vec<String> = vec![];
+
+        if let Yaml::Array(array) = &cop_config[key] {
+            for item in array {
+                if let Yaml::String(string) = item {
+                    output.push(string.clone());
+                } else {
+                    panic!("item has to be string");
+                }
+            }
+        } else {
+            panic!("the key should be array");
+        }
+
+        output
+    }
+
+    pub fn is_enabled(&self, cop: &str) -> bool {
+        let cop_config = &self.0[&Yaml::String(cop.to_string())];
+        if let Yaml::Boolean(enabled) = &cop_config["Enabled"] {
+            return *enabled;
+        } else {
+            panic!("Enabled field not found");
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::load;
+
+    #[test]
+    fn test_get_array() {
+        let config = load();
+
+        assert_eq!(
+            config.get_array("Bundler/DuplicatedGem", "Include"),
+            vec![
+                String::from("**/*.gemfile"),
+                String::from("**/Gemfile"),
+                String::from("**/gems.rb")
+            ]
+        );
+    }
+
+    #[test]
+    fn test_is_enabled() {
+        let config = load();
+
+        assert!(config.is_enabled("Bundler/DuplicatedGem"));
+        assert!(!config.is_enabled("Bundler/GemComment"));
+    }
+}
