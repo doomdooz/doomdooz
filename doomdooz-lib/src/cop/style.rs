afd28d8fac4289dd1a2acef0a823140e1887264b
Mohsen Alizadeh <mohsen@alizadeh.us>
2022-12-18 10:25:16 -0800

    add EndBlock cop

--- doomdooz-lib/src/cop/style.rs
+++ doomdooz-lib/src/cop/style.rs
@@ -2,10 +2,12 @@ pub mod alias;
 pub mod and_or;
 pub mod begin_block;
 pub mod empty_method;
+pub mod end_block;
 
 pub fn init() {
     alias::init();
     and_or::init();
     begin_block::init();
     empty_method::init();
+    end_block::init();
 }
---
+++ doomdooz-lib/src/cop/style/end_block.rs
@@ -0,0 +1,31 @@
+use crate::cop;
+use crate::cop::register_node_handler;
+use crate::source;
+use crate::types;
+
+static MSG: &str = "Avoid the use of `END` blocks. Use `Kernel#at_exit` instead.";
+static COP_NAME: &str = "Style/EndBlock";
+
+pub fn init() {
+    register_node_handler("postexe", COP_NAME, on_postexe);
+
+    cop::register(COP_NAME);
+}
+
+pub fn on_postexe(node: &types::Node, file: &source::File) {
+    if let types::Node::Postexe(node) = node {
+        file.add_offense(COP_NAME, node.keyword_l, MSG);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        crate::expect_offense!(
+            "
+            END { test }
+        "
+        );
+    }
+}
