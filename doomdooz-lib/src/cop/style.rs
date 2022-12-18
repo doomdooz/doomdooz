ca4473f591f1ecfdd2eb95876ee491ebcdb9dd29
Mohsen Alizadeh <mohsen@alizadeh.us>
2022-12-17 20:50:34 -0800

    add begin_block cop

--- doomdooz-lib/src/cop/style.rs
+++ doomdooz-lib/src/cop/style.rs
@@ -1,9 +1,11 @@
 pub mod alias;
 pub mod and_or;
+pub mod begin_block;
 pub mod empty_method;
 
 pub fn init() {
     alias::init();
     and_or::init();
+    begin_block::init();
     empty_method::init();
 }
---
+++ doomdooz-lib/src/cop/style/begin_block.rs
@@ -0,0 +1,31 @@
+use crate::cop;
+use crate::cop::register_node_handler;
+use crate::source;
+use crate::types;
+
+static MSG: &str = "Avoid the use of `BEGIN` blocks.";
+static COP_NAME: &str = "Style/BeginBlock";
+
+pub fn init() {
+    register_node_handler("preexe", COP_NAME, on_preexe);
+
+    cop::register(COP_NAME);
+}
+
+pub fn on_preexe(node: &types::Node, file: &source::File) {
+    if let types::Node::Preexe(node) = node {
+        file.add_offense(COP_NAME, node.keyword_l, MSG);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        crate::expect_offense!(
+            "
+            BEGIN { test }
+        "
+        );
+    }
+}
