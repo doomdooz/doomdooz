c0870d11f13899ebfc6582b2859a5f4cb133bef9
Mohsen Alizadeh <mohsen@alizadeh.us>
2022-12-18 19:38:28 -0800

    add layout/space_before_comma cop

--- doomdooz-lib/src/cop/layout.rs
+++ doomdooz-lib/src/cop/layout.rs
@@ -1,5 +1,7 @@
+pub mod space_before_comma;
 pub mod space_before_semicolon;
 
 pub fn init() {
+    space_before_comma::init();
     space_before_semicolon::init();
 }
---
+++ doomdooz-lib/src/cop/layout/space_before_comma.rs
@@ -0,0 +1,38 @@
+use crate::cop;
+use crate::cop::register_tokens_handler;
+use crate::source;
+use crate::types;
+
+static MSG: &str = "Space found before comma.";
+static COP_NAME: &str = "Layout/SpaceBeforeComma";
+
+pub fn init() {
+    register_tokens_handler(on_tokens, COP_NAME);
+
+    cop::register(COP_NAME);
+}
+
+pub fn on_tokens(tokens: &Vec<types::Token>, file: &source::File) {
+    let space = " ".as_bytes()[0];
+
+    for token in tokens {
+        dbg!(&token);
+        if token.token_name() == "tCOMMA" {
+            if let Some(byte) = file.parser_result.input.bytes.get(token.loc.begin - 1) {
+                if *byte == space {
+                    file.add_offense(COP_NAME, token.loc, MSG);
+                }
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        crate::expect_offense!("[1 , 2]");
+
+        crate::expect_no_offense!("[1, 2]");
+    }
+}
