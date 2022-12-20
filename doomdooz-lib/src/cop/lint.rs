e02f5028277f7a7a25bf303c45c713700b5423ae
Mohsen Alizadeh <mohsen@alizadeh.us>
2022-12-19 19:08:13 -0800

    add lin/empty_interpolation

--- doomdooz-lib/src/cop/lint.rs
+++ doomdooz-lib/src/cop/lint.rs
@@ -1,5 +1,7 @@
 pub mod duplicate_hash_key;
+pub mod empty_interpolation;
 
 pub fn init() {
     duplicate_hash_key::init();
+    empty_interpolation::init();
 }
---
+++ doomdooz-lib/src/cop/lint/empty_interpolation.rs
@@ -0,0 +1,34 @@
+use crate::cop;
+use crate::cop::register_node_handler;
+use crate::source;
+use crate::types;
+
+static MSG: &str = "Empty interpolation detected.";
+static COP_NAME: &str = "Lint/EmptyInterpolation";
+
+pub fn init() {
+    register_node_handler("dstr", COP_NAME, on_dstr);
+
+    cop::register(COP_NAME);
+}
+
+pub fn on_dstr(node: &types::Node, file: &source::File) {
+    if let types::Node::Dstr(node) = node {
+        for part in &node.parts {
+            if let types::Node::Begin(part) = part {
+                if part.statements.is_empty() {
+                    file.add_offense(COP_NAME, part.expression_l, MSG);
+                }
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        crate::expect_no_offense!(" \"something #{a}");
+        crate::expect_offense!(" \"something #{}");
+    }
+}
