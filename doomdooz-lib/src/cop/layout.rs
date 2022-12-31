d0f09be94158e130aa73e24fb9e277f84bd7fcf3
Mohsen Alizadeh <mohsen@alizadeh.us>
2022-12-30 20:10:59 -0800

    add layout/space_after_semicolon

--- doomdooz-lib/src/cop/layout.rs
+++ doomdooz-lib/src/cop/layout.rs
@@ -1,7 +1,9 @@
+pub mod space_after_semicolon;
 pub mod space_before_comma;
 pub mod space_before_semicolon;
 
 pub fn init() {
+    space_after_semicolon::init();
     space_before_comma::init();
     space_before_semicolon::init();
 }
---
+++ doomdooz-lib/src/cop/layout/space_after_semicolon.rs
@@ -0,0 +1,42 @@
+use crate::cop;
+use crate::cop::register_tokens_handler;
+use crate::source;
+use crate::types;
+
+static MSG: &str = "Space missing after semicolon.";
+static COP_NAME: &str = "Layout/SpaceAfterSemicolon";
+
+pub fn init() {
+    register_tokens_handler(on_tokens, COP_NAME);
+
+    cop::register(COP_NAME);
+}
+
+pub fn on_tokens(tokens: &Vec<types::Token>, file: &source::File) {
+    let space = " ".as_bytes()[0];
+
+    for token in tokens {
+        if token.token_name() == "tSEMI" {
+            if let Some(byte) = file.parser_result.input.bytes.get(token.loc.begin + 1) {
+                if *byte != space {
+                    file.add_offense(COP_NAME, token.loc, MSG);
+                }
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        crate::expect_offense! {"
+            x = 1;y = 2
+                 ^ Space missing after semicolon.
+        "};
+
+        crate::expect_no_offense!("x = 1; y = 2");
+
+        crate::expect_no_offense!("b = 'a ;'");
+    }
+}
