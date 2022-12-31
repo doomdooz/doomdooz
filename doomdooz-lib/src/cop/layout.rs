ac1979d416b0179b2f5f13389ecc95dea17d5188
Mohsen Alizadeh <mohsen@alizadeh.us>
2022-12-30 20:16:31 -0800

    add layout/space_after_comma

--- doomdooz-lib/src/cop/layout.rs
+++ doomdooz-lib/src/cop/layout.rs
@@ -1,8 +1,10 @@
+pub mod space_after_comma;
 pub mod space_after_semicolon;
 pub mod space_before_comma;
 pub mod space_before_semicolon;
 
 pub fn init() {
+    space_after_comma::init();
     space_after_semicolon::init();
     space_before_comma::init();
     space_before_semicolon::init();
---
+++ doomdooz-lib/src/cop/layout/space_after_comma.rs
@@ -0,0 +1,41 @@
+use crate::cop;
+use crate::cop::register_tokens_handler;
+use crate::source;
+use crate::types;
+
+static MSG: &str = "Space missing after comma.";
+static COP_NAME: &str = "Layout/SpaceAfterComma";
+
+pub fn init() {
+    register_tokens_handler(on_tokens, COP_NAME);
+
+    cop::register(COP_NAME);
+}
+
+pub fn on_tokens(tokens: &Vec<types::Token>, file: &source::File) {
+    let space = " ".as_bytes()[0];
+
+    for token in tokens {
+        if token.token_name() == "tCOMMA" {
+            if let Some(byte) = file.parser_result.input.bytes.get(token.loc.begin + 1) {
+                dbg!(*byte);
+                if *byte != space {
+                    file.add_offense(COP_NAME, token.loc, MSG);
+                }
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        crate::expect_offense! {"
+            [1,2]
+              ^ Space missing after comma.
+        "};
+
+        crate::expect_no_offense!("[1, 2]");
+    }
+}
