e4b486e8d41dff72efb4b2e87ae7bf80d0b58ff5
Mohsen Alizadeh <mohsen@alizadeh.us>
2023-01-01 12:06:16 -0800

    style/and_or missing implementation for or #6

--- doomdooz-lib/src/cop/style/and_or.rs
+++ doomdooz-lib/src/cop/style/and_or.rs
@@ -5,10 +5,12 @@ use crate::types;
 use std::str;
 
 static COP_NAME: &str = "Style/AndOr";
-const MSG: &str = "Use `&&` instead of `and`.";
+const MSG_AND: &str = "Use `&&` instead of `and`.";
+const MSG_OR: &str = "Use `||` instead of `or`.";
 
 pub fn init() {
     register_node_handler("and", COP_NAME, on_and);
+    register_node_handler("or", COP_NAME, on_or);
 
     cop::register(COP_NAME);
 }
@@ -18,7 +20,17 @@ pub fn on_and(node: &types::Node, file: &source::File) {
         let operator = file.source(node.operator_l);
 
         if operator == "and" {
-            file.add_offense(COP_NAME, node.operator_l, MSG);
+            file.add_offense(COP_NAME, node.operator_l, MSG_AND);
+        }
+    }
+}
+
+pub fn on_or(node: &types::Node, file: &source::File) {
+    if let types::Node::Or(node) = node {
+        let operator = file.source(node.operator_l);
+
+        if operator == "or" {
+            file.add_offense(COP_NAME, node.operator_l, MSG_OR);
         }
     }
 }
@@ -31,6 +43,12 @@ mod tests {
                  ^^^ Use `&&` instead of `and`.
         "};
 
+        crate::expect_offense! {"
+            true or false
+                 ^^ Use `||` instead of `or`.
+        "};
+
         crate::expect_no_offense!("true && false");
+        crate::expect_no_offense!("true || false");
     }
 }
