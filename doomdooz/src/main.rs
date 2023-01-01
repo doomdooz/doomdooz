47b85fc5ce191e71e5839175c888c422126b23fb
Mohsen Alizadeh <mohsen@alizadeh.us>
2023-01-01 12:26:40 -0800

    add clap to the project #2

--- doomdooz/src/main.rs
+++ doomdooz/src/main.rs
@@ -1,6 +1,8 @@
 use doomdooz_lib::{cop, source, target_finder, COPS};
 use rayon::prelude::*;
 
+use clap::{command, Arg, ArgAction};
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     cop::init();
 
@@ -9,6 +11,20 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             "there are {} cops implemented so far",
             COPS.lock().unwrap().len()
         );
+    }
+
+    let matches = command!() // requires `cargo` feature
+        .arg(Arg::new("file").action(ArgAction::Append))
+        .get_matches();
+
+    let files = matches
+        .get_many::<String>("file")
+        .unwrap_or_default()
+        .map(|v| v.as_str())
+        .collect::<Vec<_>>();
+
+    if files.len() > 0 {
+        panic!("passing files as argument is not supported yet");
     }
 
     // too slow
